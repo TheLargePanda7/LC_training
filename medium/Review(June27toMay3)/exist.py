28c2f1ef3c68d44c765afe81ceb70510b5e715b8
TheLargePanda7 <107139614+TheLargePanda7@users.noreply.github.com>
2022-07-09 12:21:23 -0400

    Finished reviewing problem 79

--- medium/Review(June27toMay3)/exist.py
+++ medium/Review(June27toMay3)/exist.py
@@ -6,6 +6,7 @@ class Solution:
             rows = len(board)
             # Base Case
             if len(prefix) == 0:
+                # Prefix gets truncated as we traverse. If we are down to empty string, then a path of the given word is found
                 return True
             
             # Boundary Checking
@@ -17,7 +18,8 @@ class Solution:
             South = dfs(r+1,c,prefix[1:]) # South
             East = dfs(r,c-1,prefix[1:]) # West
             West = dfs(r,c+1,prefix[1:]) # East
-            # Backtracking
+            # Backtracking (Must revert back to original state after searching)
+            # Otherwise, if other path visits this cell again with a value of #, we will not be able to continue our traversal
             board[r][c] = prefix[0]
             
             return North or South or East or West
