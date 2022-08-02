7decea7489ce88737885dd89348b9a81b3d6b790
TheLargePanda7 <107139614+TheLargePanda7@users.noreply.github.com>
2022-08-02 10:51:37 -0400

    Finished reviewing 1151. Minimum Swaps to Group All 1's Together

--- medium/intermediate/minSwaps.py
+++ medium/intermediate/minSwaps.py
@@ -19,11 +19,13 @@ class Solution(object):
             R += 1
             
             size_of_curr_wind = (R - L)
+            # The size of window/subarray exceeds what we want, need to adjust left ptr to keep the size of number ones in original array
             if size_of_curr_wind > expect_ones:
                 offset = data[L]
                 L += 1
                 currOnes -= offset
-            
+                
+            # Once everything is update (including the size of current window), see if the number ones in this current window is any better than the previous one we computed
             maxOnes = max(maxOnes,currOnes)
             
         return expect_ones - maxOnes
