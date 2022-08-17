0faf7b34004726c4a3a3ed339e9b953e3a1e3a05
TheLargePanda7 <107139614+TheLargePanda7@users.noreply.github.com>
2022-08-17 19:10:47 -0400

    Finished reviewing 22. Generate Parentheses

--- medium/generateParenthesis.py
+++ medium/generateParenthesis.py
@@ -1,43 +1,25 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         
-        # Keep track of the count of open and close parenthesis
-        open_parenthesis_cnt = 0
-        close_parenthesis_cnt = 0
-        res = []
-
-        def backtrack(curr):
-            nonlocal open_parenthesis_cnt, close_parenthesis_cnt
-            if len(curr) == n * 2:
-                res.append("".join(curr.copy()))
+        def backtrack(res,arr,n,openCnt,closeCnt):
+            #nonlocal openCnt, closeCnt
+            # Base case
+            if openCnt == n and closeCnt == n:
+                res.append("".join(arr.copy()))
                 return
             
-            # At each position, we have two choices: "(" or ")"
-            
-            # First chpoce "("
-            open_parenthesis_cnt += 1
-            # If the cnt of close parenthesis is greater than open parenthesis, it is not in proper form
-            if open_parenthesis_cnt >= close_parenthesis_cnt and open_parenthesis_cnt <= n:
-                curr.append("(")
-                backtrack(curr)
-                # Backtrack to pick a second choice
-                curr.pop()
-            open_parenthesis_cnt -= 1
-            
-            # Second choice ")"
-            close_parenthesis_cnt += 1
-            # If the cnt of close parenthesis is greater than open parenthesis, it is not in proper form
-            if open_parenthesis_cnt >= close_parenthesis_cnt and close_parenthesis_cnt <= n:
-                curr.append(")")
-                backtrack(curr)
-                curr.pop()
-            
-            close_parenthesis_cnt -= 1
-            
-            
-            
-            
-        curr = []
-        backtrack(curr)
+            if openCnt < n:
+                arr.append("(")
+                backtrack(res,arr,n,openCnt + 1,closeCnt)
+                arr.pop()
+                
+            if closeCnt < openCnt:
+                arr.append(")")
+                backtrack(res,arr,n,openCnt,closeCnt + 1)
+                arr.pop() # Important
+                
+        res = []
+        arr = []
+        backtrack(res,arr,n,0,0)
         
         return res
